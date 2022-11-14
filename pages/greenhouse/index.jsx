14c59e2c8eca404c01acee34058fee21e2071534
Aryan Gaur <gaur.aryan.58@gmail.com>
2022-11-14 11:00:28 -0500

    style: greenhouse screen

---
+++ pages/greenhouse/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import GreenHouse from "../../components/greenhouse/greenhouse";
+
+const GreenHousePage = () => {
+  return (
+    <Box>
+      <GreenHouse />
+    </Box>
+  );
+};
+
+export default GreenHousePage;
